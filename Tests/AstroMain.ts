f363dd863a7443d20958111951141bb18abc7b49
Steve Akinyemi <appcypher@outlook.com>
2017-08-24 16:52:34 +0100

    fix paths

--- Tests/AstroMain.ts
+++ Tests/AstroMain.ts
@@ -3,11 +3,12 @@ import {Lexer}   from "../AstroCompiler/AstroLexer"
 import {Parser}  from "../AstroCompiler/AstroParser"
 import {Utility} from "../AstroCompiler/AstroUtility"
 
-let fileName1 = './test1.ast';
-let fileName2 = './test2.ast';
-let fileName3 = './test3.ast';
-let fileName4 = './test4.ast';
-let fileNameP = './parser_test1.ast';
+// paths relative to root folder, that's where npm script test starts from.
+let fileName1 = 'Tests/test1.ast';
+let fileName2 = 'Tests/test2.ast';
+let fileName3 = 'Tests/test3.ast';
+let fileName4 = 'Tests/test4.ast';
+let fileNameP = 'Tests/parser_test1.ast';
 
 fs.readFile(fileNameP, function (err, data) {
     if (err) { return console.error(err); }
