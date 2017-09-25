e8d41c66ce74621d06c4e35693321125408beb20
Steve Akinyemi <appcypher@outlook.com>
2017-09-25 12:02:58 +0100

    add comments and better error message

--- AstroCompiler/PEG/parser.js
+++ AstroCompiler/PEG/parser.js
@@ -2,14 +2,27 @@
 const peg = require("pegjs");
 const fs  = require("fs");
 
+// using print function for convenience
 let print = (s) => console.log(s);
 
+// read astro grammar file
 const grammar = fs.readFileSync("./astro.pegjs", "utf8");
 
-const parser  = peg.generate(grammar);
+// generate paser from grammar
+const parser = peg.generate(grammar);
 print("== Parser Tree Generated Successfully! ==");
 
-const code    = fs.readFileSync("./code.ast", "utf8");
-parser.parse(code);
-print("== Code Parsed Successfully! ==");
+// read sample code file
+const code = fs.readFileSync("./code.ast", "utf8");
+
+// parse sample code
+try {
+  const result = parser.parse(code);
+  print("== Code Parsed Successfully! ==");
+  print(`result: { ${result} }`);
+}
+catch (err) {
+  print(`Error!: ${err.message}`);
+  print(err.location);
+}
 
