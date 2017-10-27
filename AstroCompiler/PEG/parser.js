d1de35701c125d2a8210774360db972c6295050d
Steve Akinyemi <appcypher@outlook.com>
2017-10-27 15:05:28 +0100

    make parser generation error clearer (getting the line-col number info)

--- AstroCompiler/PEG/parser.js
+++ AstroCompiler/PEG/parser.js
@@ -9,21 +9,29 @@ function print(...s) { (s[0] instanceof Array || typeof s[0] === 'object' ) ? co
 // read astro grammar file
 const grammar = fs.readFileSync(path.join(__dirname, "./astro.pegjs"), "utf8");
 
-// generate paser from grammar
-const parser = peg.generate(grammar);
-print("== Parser Generated Successfully! ==");
-
-// read sample code file
-const code = fs.readFileSync(path.join(__dirname, "./tempshort.ast"), "utf8");
-
-// parse sample code
 try {
-  const result = parser.parse(code);
-  print("== Code Parsed Successfully! ==");
-  print(result);
+  // generate paser from grammar
+  const parser = peg.generate(grammar);
+  
+  print("== Parser Generated Successfully! ==");
+
+  // read sample code file
+  const code = fs.readFileSync(path.join(__dirname, "./tempshort.ast"), "utf8");
+    
+  // parse sample code
+  try {
+    const result = parser.parse(code);
+    print("== Code Parsed Successfully! ==");
+    print(result);
+  }
+  catch (err) {
+    print(`Error!: ${err.message}`);
+    print(err.location);
+  }
 }
 catch (err) {
   print(`Error!: ${err.message}`);
   print(err.location);
 }
+
 
