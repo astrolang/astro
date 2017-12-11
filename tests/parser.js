38cdd1ad8c6686ddf7d7601dc1cf7197e62a9f35
Steve Akinyemi <appcypher@outlook.com>
2017-12-11 21:04:34 +0100

    Add tests boilerplate

---
+++ tests/parser.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+
+chai.should();
+
+describe('Parser', () => {
+  it('should return an object if variable subject declaration is well formed');
+  it('should return an object if constant subject declaration is well formed');
+});
