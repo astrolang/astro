e5e61ebbc2ded32f38ffb4cb0978e8acb0b01f96
Steve Akinyemi <appcypher@outlook.com>
2017-12-14 18:12:16 +0100

    Add dummy test file for fibonacci sample

---
+++ tests/samples/fibonacci.spec.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+chai.should();
+
+describe('Lexer', () => {
+  it('should return an object if variable subject declaration is well formed');
+  it('should return an object if constant subject declaration is well formed');
+});
+
