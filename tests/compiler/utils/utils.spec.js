313a4c517361022fc5cad10bf08231887f1ca7d0
Steve Akinyemi <appcypher@outlook.com>
2017-12-15 00:49:15 +0100

    Refactor to follow airbnb style guide

--- tests/compiler/utils/utils.spec.js
+++ tests/compiler/utils/utils.spec.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
-import { print, clean } from '../../../src/compiler/utils';
 import cap from 'capture-console';
+import { print, clean } from '../../../src/compiler/utils';
 
 chai.should();
 
@@ -13,7 +13,7 @@ describe('Compiler:Utils', () => {
     result.should.equal('[\n  1,\n  2,\n  3,\n  4,\n  5\n]\n');
     done();
   });
-  
+
   it('print function should pretty print an object passed as an argument', (done) => {
     const sample = { name: 'Jane', age: 25 };
     const result = cap.captureStdout(() => {
@@ -22,7 +22,7 @@ describe('Compiler:Utils', () => {
     result.should.equal('{\n  "name": "Jane",\n  "age": 25\n}\n');
     done();
   });
-  
+
   it('print function should print a string passed as an argument', (done) => {
     const sample = 'Hello world!';
     const result = cap.captureStdout(() => {
@@ -31,7 +31,7 @@ describe('Compiler:Utils', () => {
     result.should.equal('Hello world!\n');
     done();
   });
-  
+
   it('print function should join and print multiple arguments passed to it', (done) => {
     const result = cap.captureStdout(() => {
       print('person = ', { name: 'John', age: 45 });
@@ -39,7 +39,7 @@ describe('Compiler:Utils', () => {
     result.should.equal('person = {"name":"John","age":45}\n');
     done();
   });
-  
+
   it('clean function should remove nulls and undefined values from array', (done) => {
     const sample = [1, 2, null, 3, undefined, 4, 5];
     const result = clean(sample);
