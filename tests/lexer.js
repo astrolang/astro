ea957b77c00496e3a452d540fad5ff0eba432dff
Steve Akinyemi <appcypher@outlook.com>
2017-12-14 17:35:00 +0100

    Remove old dummy test files

--- tests/lexer.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-
-chai.should();
-
-describe('Lexer', () => {
-  it('should return an object if variable subject declaration is well formed');
-  it('should return an object if constant subject declaration is well formed');
-});
-
--- tests/parser.js
+++
@@ -1,8 +0,0 @@
-import chai from 'chai';
-
-chai.should();
-
-describe('Parser', () => {
-  it('should return an object if variable subject declaration is well formed');
-  it('should return an object if constant subject declaration is well formed');
-});
