bea9f7c54439a9c0fba202003c7d71228cdea8ca
Steve Akinyemi <appcypher@outlook.com>
2017-12-15 11:38:20 +0100

    Refactor code to follow airbnb style guide

--- src/compiler/utils/index.js
+++ src/compiler/utils/index.js
@@ -1,19 +1,19 @@
 // Stringifies and pretty prints all kinds of values including arrays and objects
 export const print = (...s) => {
-  if (s.length == 1) {
-    if (typeof(s[0]) === 'string') {
+  if (s.length === 1) {
+    if (typeof (s[0]) === 'string') {
       console.log(s[0]);
     } else {
       console.log(JSON.stringify(s[0], null, 2));
     }
   } else {
     const result = s
-      .map(e => typeof(e) === 'string' ? e : JSON.stringify(e))
+      .map(e => (typeof (e) === 'string' ? e : JSON.stringify(e)))
       .join('');
     console.log(result);
   }
 };
 
 // Removes null or undefined values from an array
-export const clean = (a) => a.filter(x => x != undefined);
+export const clean = a => a.filter(x => x != null);
 
