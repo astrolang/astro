9866fb21670bbf75f0d5310d1a95b16c41981476
Steve Akinyemi <appcypher@outlook.com>
2017-12-29 02:58:14 +0100

    Add function printing to `print` function

--- src/compiler/utils/index.js
+++ src/compiler/utils/index.js
@@ -4,6 +4,8 @@ const print = (...s) => {
   if (s.length === 1) {
     if (typeof (s[0]) === 'string') {
       console.log(s[0]);
+    } else if (s[0] instanceof Function) {
+      console.log(s[0].toString());
     } else {
       console.log(JSON.stringify(s[0], null, 2));
     }
