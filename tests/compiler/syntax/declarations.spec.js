fc1cde83b2b1d965ceeade9581e1e89270092eca
Steve Akinyemi <appcypher@outlook.com>
2017-12-29 01:42:13 +0100

    Add dummy tests for language declaration syntax

---
+++ tests/compiler/syntax/declarations.spec.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+
+chai.should();
+
+describe('COMPILER::SYNTAX::DECLARATIONS', () => {
+  describe('Parser.parse', () => {
+    it('should do x');
+
+    it('should do y');
+  });
+});
+
