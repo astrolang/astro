50bae3bf042aa0ffda33ba2037fbc92d8679fa29
Steve Akinyemi <appcypher@outlook.com>
2018-01-21 22:42:45 +0100

    Add fail test samples

--- src/compiler/syntax/test4.js
+++ src/compiler/syntax/test4.js
@@ -4,11 +4,14 @@ const Parser = require('./parser4');
 
 print(new Parser('name  nom').parseIdentifier());
 print(new Parser('name_ 5').parseIdentifier());
+print(new Parser(' name').parseIdentifier()); // fail
+print(new Parser('').parseIdentifier()); // fail
 print(new Parser('_na99_me_ age').parseIdentifier());
 print(new Parser('_na99_me_ age').parseIdentifier());
 print(new Parser('99').parseInteger());
 print(new Parser('9978').parseInteger());
 print(new Parser('997890e747').parseInteger());
+print(new Parser(' 997890e747').parseInteger()); // fail
 print(new Parser('\t\t997890e747').parseWhitespaces());
 print(new Parser(' \t997890e747').parseWhitespaces());
 print(new Parser('     997890e747').parseWhitespaces());
@@ -19,6 +22,6 @@ print(new Parser('name').parseExpression());
 print(new Parser('4455').parseExpression());
 print(new Parser('let name = 678').parseSubjectDeclaration());
 print(new Parser('var _n77m_m = age').parseSubjectDeclaration());
-// print(new Parser(' 997890e747').parseInteger());
-// print(new Parser(' name').parseIdentifier());
-// print(new Parser('').parseIdentifier());
+print(new Parser('var 4 = age').parseSubjectDeclaration()); // fail
+print(new Parser('mut x = age').parseSubjectDeclaration()); // fail
+print(new Parser('mut x = $$$').parseSubjectDeclaration()); // fail
