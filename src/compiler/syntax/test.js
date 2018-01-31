febad055c61c7e42ac901664a39f63b44d1ab5c4
Steve Akinyemi <appcypher@outlook.com>
2018-01-31 14:55:04 +0100

    Make fail tests more apparent

--- src/compiler/syntax/test.js
+++ src/compiler/syntax/test.js
@@ -2,27 +2,26 @@ const { print } = require('../utils');
 const Parser = require('./parser');
 
 print('========= IDENTIFIER =========');
+print(' name>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
+print(new Parser(' name').parseIdentifier()); // fail
+print('>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
+print(new Parser('').parseIdentifier()); // fail
+print('_na99_me_ age');
 print('name  nom');
 print(new Parser('name  nom').parseIdentifier());
 print('name_ 5');
 print(new Parser('name_ 5').parseIdentifier());
-print(' name'); // fail
-print(new Parser(' name').parseIdentifier()); // fail
-print(''); // fail
-print(new Parser('').parseIdentifier()); // fail
-print('_na99_me_ age');
-print(new Parser('_na99_me_ age').parseIdentifier());
 print('_na99_me_ age');
 print(new Parser('_na99_me_ age').parseIdentifier());
 print('========= INTEGER =========');
+print(' 997890e747>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
+print(new Parser(' 997890e747').parseIntegerDecimalLiteral()); // fail
 print('99');
-print(new Parser('99').parseInteger());
+print(new Parser('99').parseIntegerDecimalLiteral());
 print('9978');
-print(new Parser('9978').parseInteger());
+print(new Parser('9978').parseIntegerDecimalLiteral());
 print('997890e747');
-print(new Parser('997890e747').parseInteger());
-print(' 997890e747'); // fail
-print(new Parser(' 997890e747').parseInteger()); // fail
+print(new Parser('997890e747').parseIntegerDecimalLiteral());
 print('========= SPACES =========');
 print('\t\t997890e747');
 print(new Parser('\t\t997890e747').parseSpaces());
@@ -43,30 +42,30 @@ print(new Parser('name').parseExpression());
 print('4455');
 print(new Parser('4455').parseExpression());
 print('========= SUBJECT DECLARATION =========');
-print('var 4 = age'); // fail
+print('var 4 = age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('var 4 = age').parseSubjectDeclaration()); // fail
-print('mut x = age'); // fail
+print('mut x = age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('mut x = age').parseSubjectDeclaration()); // fail
-print('let x = $$$'); // fail
+print('let x = $$$>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('let x = $$$').parseSubjectDeclaration()); // fail
-print('var nim =age'); // fail
+print('var nim =age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('var nim =age').parseSubjectDeclaration()); // fail
-print('var nim= age'); // fail
+print('var nim= age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('var nim= age').parseSubjectDeclaration()); // fail
 print('let name = 678');
 print(new Parser('let name = 678').parseSubjectDeclaration());
 print('var _n77m_m=age');
 print(new Parser('var _n77m_m=age').parseSubjectDeclaration());
 print('========= FUNCTION DECLARATION =========');
-print('fun 4() = age'); // fail
+print('fun 4() = age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('fun 4() = age').parseFunctionDeclaration()); // fail
-print('mut x() = age'); // fail
+print('mut x() = age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('mut x() = age').parseFunctionDeclaration()); // fail
-print('fun x() = $$$'); // fail
+print('fun x() = $$$>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('fun x() = $$$').parseFunctionDeclaration()); // fail
-print('fun nim() =age'); // fail
+print('fun nim() =age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('fun nim() =age').parseFunctionDeclaration()); // fail
-print('fun nim()= age'); // fail
+print('fun nim()= age>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('fun nim()= age').parseFunctionDeclaration()); // fail
 print('fun name() = 678');
 print(new Parser('fun name() = 678').parseFunctionDeclaration());
@@ -75,9 +74,9 @@ print(new Parser('fun _n77m_m()=age').parseFunctionDeclaration());
 print('fun _n77m_m ( ) = age');
 print(new Parser('fun _n77m_m ( ) = age').parseFunctionDeclaration());
 print('========= NAMES =========');
-print(',some'); // fail
+print(',some>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser(',some').parseNames()); // fail
-print('20, 45'); // fail
+print('20, 45>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('20, 45').parseNames()); // fail
 print('some');
 print(new Parser('some').parseNames());
@@ -86,7 +85,7 @@ print(new Parser('the,new,age').parseNames());
 print('the, _ew,_007');
 print(new Parser('the, _ew,_007').parseNames());
 print('========= TYPE DECLARATION =========');
-print('typePerson'); // fail
+print('typePerson>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('typePerson').parseTypeDeclaration()); // fail
 print('type person(): var name = 4');
 print(new Parser('type person(): var name = 4').parseTypeDeclaration());
@@ -99,7 +98,7 @@ print(new Parser('type Hx99 <: Human, Animal: var age = 6').parseTypeDeclaration
 print('type Waiter ( ) <: Human, Employee');
 print(new Parser('type Waiter ( ) <: Human, Employee').parseTypeDeclaration());
 print('========= INDENT =========');
-print('     •'); // fail
+print('     •>>>>>>>>>>>>>>>>>>>>>>>>FAIL'); // fail
 print(new Parser('     •').parseIndent()); // fail
 print('    •');
 print(new Parser('    *').parseIndent());
