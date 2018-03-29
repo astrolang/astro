7d2d1c195e61bffcf90f3f5f705c52a13016e580
Steve Akinyemi <appcypher@outlook.com>
2018-03-29 22:10:21 +0100

    Add some lexer tests

--- src/compiler/syntax/lexer-test.js
+++ src/compiler/syntax/lexer-test.js
@@ -1,16 +1,108 @@
 const Lexer = require('./lexer');
-const { print } = require('../utils');
-const assert = require('assert');
+const { print, createTest } = require('../utils');
 
 // Test object.
 let lexer = null;
+let result = null;
+const test = createTest();
 
 print('============== SPACES ==============');
-print(String.raw` \t\t  `);
 lexer = new Lexer(' \t\t  ');
-assert(lexer, {
-  token: '',
-  kind: 'spaces',
-  line: 1,
-  column: 0,
-});
+result = lexer.spaces();
+test(
+  String.raw` \t\t  `,
+  result,
+  {
+    token: '',
+    kind: 'spaces',
+    line: 1,
+    column: 0,
+  },
+);
+
+
+lexer = new Lexer('');
+result = lexer.spaces();
+test(
+  String.raw``,
+  result,
+  null,
+);
+
+print('============== NONAME ==============');
+lexer = new Lexer('_');
+result = lexer.noName();
+test(
+  String.raw`_`,
+  result,
+  {
+    token: '_',
+    kind: 'noname',
+    line: 1,
+    column: 0,
+  },
+);
+
+print('============== IDENTIFIER ==============');
+lexer = new Lexer('some_name678');
+result = lexer.identifier();
+test(
+  String.raw`some_name678`,
+  result,
+  {
+    token: 'some_name678',
+    kind: 'identifier',
+    line: 1,
+    column: 0,
+  },
+);
+
+lexer = new Lexer('678name');
+result = lexer.identifier();
+test(
+  String.raw`678name`,
+  result,
+  null,
+);
+
+lexer = new Lexer('where');
+result = lexer.identifier();
+test(
+  String.raw`where`,
+  result,
+  {
+    token: 'where',
+    kind: 'keyword',
+    line: 1,
+    column: 0,
+  },
+);
+
+lexer = new Lexer('else');
+result = lexer.identifier();
+test(
+  String.raw`else`,
+  result,
+  {
+    token: 'else',
+    kind: 'keyword',
+    line: 1,
+    column: 0,
+  },
+);
+
+lexer = new Lexer('elseif');
+result = lexer.identifier();
+test(
+  String.raw`elseif`,
+  result,
+  {
+    token: 'elseif',
+    kind: 'identifier',
+    line: 1,
+    column: 0,
+  },
+);
+
+// Print details of test.
+test();
