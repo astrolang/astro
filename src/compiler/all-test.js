8d6438a5121304578b4a033fc9dfa1d47a5a9001
Steve Akinyemi <appcypher@outlook.com>
2018-05-27 23:18:30 +0100

    Refactor import order

--- src/compiler/all-test.js
+++ src/compiler/all-test.js
@@ -1,6 +1,6 @@
+const { showTestInfo } = require('./utils');
 const indexTest = require('./utils/index-test');
 const lexerTest = require('./syntax/lexer-test');
 const parserTest = require('./syntax/parser-test');
-const { showTestInfo } = require('./utils');
 
 showTestInfo(indexTest, lexerTest, parserTest);
