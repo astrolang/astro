e94298d7b6134d3656136887f8f10057e5f73fe8
Steve Akinyemi <appcypher@outlook.com>
2018-05-28 01:48:50 +0100

    Add prompt modes

--- bin/astro.js
+++ bin/astro.js
@@ -9,9 +9,10 @@ _______________________________________
 |  /  |  |\x1b[35m__\x1b[0m \\__'   \x1b[35m__\x1b[0m/|   /  |  | |  |
 |  \\__|  |__\\_  |  |___|  |   |  |_|  |
  \\\x1b[35m____\x1b[0m'\x1b[35m__\x1b[0m|\x1b[35m______\x1b[0m/\\\x1b[35m_____\x1b[0m|\x1b[35m__\x1b[0m|   |\x1b[35m_______\x1b[0m/
-_______________________________________
+___________ \x1b[2m\x1b[39mtype ? for help\x1b[0m ___________
 `;
 const astroCodePrompt = '\x1b[1m\x1b[35m•••• \x1b[0m';
+const astroHelpPrompt = '\x1b[1m\x1b[32m?••• \x1b[0m';
 
 // Console Interface.
 print(astroASCIIDesign);
@@ -30,6 +31,12 @@ prompt.on('line', (response) => {
     switch(response.trim()) {
         case 'exit':
             prompt.close();
+            break;
+        case '?':
+            prompt.setPrompt(astroHelpPrompt);
+            break;
+        case ':':
+            prompt.setPrompt(astroCodePrompt);
             break;
         default:
             const lexer = new Lexer(response);
