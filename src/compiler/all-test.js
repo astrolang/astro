f6fe94a92dd4e2c28ffca447904f009b585868a4
Steve Akinyemi <appcypher@outlook.com>
2018-06-09 11:09:14 +0100

    Add the new separate parser files

--- src/compiler/all-test.js
+++ src/compiler/all-test.js
@@ -1,6 +1,7 @@
 const { showTestInfo } = require('./utils');
 const indexTest = require('./utils/index-test');
 const lexerTest = require('./syntax/lexer-test');
-const parserTest = require('./syntax/parser-test');
+const parserTest1 = require('./syntax/parser-test-1');
+const parserTest2 = require('./syntax/parser-test-2');
 
-showTestInfo(indexTest, lexerTest, parserTest);
+showTestInfo(indexTest, lexerTest, parserTest1, parserTest2);
