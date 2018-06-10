770dddb61b00a8517ce1b060a00e8557190e619d
Steve Akinyemi <appcypher@outlook.com>
2018-06-10 14:59:21 +0100

    Add encompassing test file for all parser-related tests

---
+++ src/compiler/syntax/parser-tests.js
@@ -0,0 +1,5 @@
+const { showTestInfo } = require('../utils');
+const parserTest1 = require('./parser-test-1');
+const parserTest2 = require('./parser-test-2');
+
+showTestInfo(parserTest1, parserTest2);
