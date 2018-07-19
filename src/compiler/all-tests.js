bbea15a83fc57ed21ffeb7bb65f6a34453a65582
Steve Akinyemi <appcypher@outlook.com>
2018-07-19 18:38:15 +0100

    Add new test files

--- src/compiler/all-tests.js
+++ src/compiler/all-tests.js
@@ -3,5 +3,6 @@ const indexTest = require('../utils/index-test');
 const lexerTest = require('./syntax/lexer-test');
 const parserTest1 = require('./syntax/parser-test-1');
 const parserTest2 = require('./syntax/parser-test-2');
+const parserTest3 = require('./syntax/parser-test-3');
 
-showTestInfo(indexTest, lexerTest, parserTest1, parserTest2);
+showTestInfo(indexTest, lexerTest, parserTest1, parserTest2, parserTest3);
