1de3e1116096597cad67957b25952105f02cd3bd
Steve Akinyemi <appcypher@outlook.com>
2018-07-19 02:24:10 +0100

    Add additional tests

--- src/compiler/syntax/parser-tests.js
+++ src/compiler/syntax/parser-tests.js
@@ -1,5 +1,6 @@
-const { showTestInfo } = require('../utils');
+const { showTestInfo } = require('../../utils');
 const parserTest1 = require('./parser-test-1');
 const parserTest2 = require('./parser-test-2');
+const parserTest3 = require('./parser-test-3');
 
-showTestInfo(parserTest1, parserTest2);
+showTestInfo(parserTest1, parserTest2, parserTest3);
