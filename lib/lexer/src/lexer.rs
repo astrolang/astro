06e72431a30fc9382d2b2898bc789ca4dccfec2c
Steve Akinyemi <appcypher@outlook.com>
2019-01-02 14:27:56 +0100

    Implement space lexing

--- lib/lexer/src/lexer.rs
+++ lib/lexer/src/lexer.rs
@@ -1,10 +1,50 @@
+#[derive(Debug, Clone, PartialEq)]
 pub struct Token {
-    kind: String,
+    kind: TokenKind,
     token: Option<String>,
     cursor: usize,
-    errno: usize,
 }
 
+impl Token {
+    pub fn new(kind: TokenKind, token: Option<String>, cursor: usize) -> Self {
+        Self {
+            kind,
+            token,
+            cursor,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenKind {
+    Spaces,
+    Newline,
+    LineContinuation,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LexerError {
+    error: Error,
+    kind: TokenKind,
+    cursor: usize,
+}
+
+impl LexerError {
+    pub fn new(error: Error, kind: TokenKind, cursor: usize) -> Self {
+        Self {
+            error,
+            cursor,
+            kind,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Error {
+    CantConsume,
+}
+
+#[derive(Debug, Clone)]
 pub struct Lexer {
     code: Vec<char>,
     cursor: usize,
@@ -23,7 +63,7 @@ pub struct Lexer {
 
 impl Lexer {
     /// Creates a new lexer object from the code passed in.
-    fn new(code: String) -> Self {
+    pub fn new(code: String) -> Self {
         Self {
             code: code.chars().collect(),
             cursor: 0,
@@ -33,25 +73,54 @@ impl Lexer {
             digit_octal: String::from("01234567"),
             digit_decimal: String::from("0123456789"),
             digit_hexadecimal: String::from("0123456789ABCDEFabcdef"),
-            identifier_begin_char: String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"), // TODO: Unicode
-            identifier_end_char: String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"), // TODO: Unicode
+            identifier_begin_char: String::from(
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_",
+            ), // TODO: Unicode
+            identifier_end_char: String::from(
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789",
+            ), // TODO: Unicode
             operator_char: String::from(":+'-*/\\^%&|!><=÷×≠≈¹²³√?~"), // TODO: Unicode
-            punctuator_char: String::from("(){}[],.;@$"), // TODO: Unicode
+            punctuator_char: String::from("(){}[],.;@$"),                         // TODO: Unicode
             keywords: vec![
-                String::from("import"), String::from("export"), String::from("let"), String::from("var"),
-                String::from("const"), String::from("fun"), String::from("type"), String::from("async"),
-                String::from("ref"), String::from("iso"), String::from("if"), String::from("elif"),
-                String::from("else"), String::from("except"), String::from("ensure"), String::from("defer"),
-                String::from("loop"), String::from("end"), String::from("fallthrough"), String::from("return"),
-                String::from("raise"), String::from("break"), String::from("continue"), String::from("yield"),
-                String::from("from"), String::from("await"), String::from("where"), String::from("is"),
-                String::from("not"), String::from("in"), String::from("as"), String::from("mod"),
-                String::from("typeof"), String::from("super"),
+                String::from("import"),
+                String::from("export"),
+                String::from("let"),
+                String::from("var"),
+                String::from("const"),
+                String::from("fun"),
+                String::from("type"),
+                String::from("async"),
+                String::from("ref"),
+                String::from("iso"),
+                String::from("if"),
+                String::from("elif"),
+                String::from("else"),
+                String::from("except"),
+                String::from("ensure"),
+                String::from("defer"),
+                String::from("loop"),
+                String::from("end"),
+                String::from("fallthrough"),
+                String::from("return"),
+                String::from("raise"),
+                String::from("break"),
+                String::from("continue"),
+                String::from("yield"),
+                String::from("from"),
+                String::from("await"),
+                String::from("where"),
+                String::from("is"),
+                String::from("not"),
+                String::from("in"),
+                String::from("as"),
+                String::from("mod"),
+                String::from("typeof"),
+                String::from("super"),
             ],
         }
     }
 
-    /// Returns the next character in the code but does not consume it.
+    /// Returns the next character in code but does not consume it.
     fn peek_char(&self, offset: Option<usize>) -> Option<char> {
         let offset = offset.unwrap_or(0);
         if self.is_inbounds(Some(offset)) {
@@ -60,7 +129,7 @@ impl Lexer {
         None
     }
 
-    /// Returns the specified token if it is next in the code but does not consume it.
+    /// Returns the specified token if it is next in code but does not consume it.
     fn peek_token(&self, token: String) -> bool {
         let start = self.cursor;
         let end = start + token.len();
@@ -68,13 +137,13 @@ impl Lexer {
         let token_chars: Vec<char> = token.chars().collect();
         let token_chars = token_chars.as_slice();
         if slice == token_chars {
-            return true
+            return true;
         }
         false
     }
 
-    /// Consumes the specified token if it is next in the code
-    fn eat_token(&mut self, token: String) ->  Option<String> {
+    /// Consumes the specified token if it is next in code
+    fn eat_token(&mut self, token: String) -> Option<String> {
         let start = self.cursor;
         let end = start + token.len();
         let slice = &self.code[start..end];
@@ -83,12 +152,12 @@ impl Lexer {
         if slice == token_chars {
             // Advance cursor.
             self.cursor += 1;
-            return Some(token)
+            return Some(token);
         }
         None
     }
 
-    /// Consumes the next character in the code.
+    /// Consumes the next character in code.
     fn eat_char(&mut self) -> char {
         let character = self.code[self.cursor];
         // Advance cursor.
@@ -106,12 +175,11 @@ impl Lexer {
         false
     }
 
-    /// Consumes the next spaces in the code.
-    fn spaces(&mut self) -> Option<Token> {
-        let kind = String::from("spaces");
+    /// Consumes the next spaces in code.
+    fn spaces(&mut self) -> Result<Token, LexerError> {
+        let kind = TokenKind::Spaces;
         let token: Option<String> = None;
-        let errno = 0;
-        let Lexer { cursor, .. } = self;
+        let cursor = self.cursor;
         let mut count = 0;
 
         // Consume available spaces.
@@ -121,22 +189,64 @@ impl Lexer {
                 self.eat_char();
                 count += 1;
             } else {
-                break
+                break;
             }
         }
 
         // Revert if lexing failed.
         if count == 0 {
-            self.cursor = *cursor;
-            return None;
+            self.cursor = cursor;
+            return Err(LexerError::new(Error::CantConsume, kind, cursor));
         }
 
-        // Return Token.
-        Some(Token {
-            kind, token, cursor: *cursor, errno
-        })
+        Ok(Token::new(kind, token, cursor))
     }
 
+    /// Consume the next newline in code.
+    fn newline(&mut self) -> Result<Token, LexerError> {
+        let kind = TokenKind::Newline;
+        let token: Option<String> = None;
+        let cursor = self.cursor;
+
+        // Get the next two chararcters.
+        let character = self.peek_char(None);
+        let character2 = self.peek_char(Some(1));
+
+        // Consume any following newlines.
+        if character.is_some() && character.unwrap() == '\n' { // Unix newline
+            self.eat_char();
+        } else if character.is_some() && character.unwrap() == '\r' && character2.unwrap() == '\n' { // Windows newline
+            self.eat_char();
+            self.eat_char();
+        } else { // Otherwise
+            return Err(LexerError::new(Error::CantConsume, kind, cursor));
+        }
+
+        Ok(Token::new(kind, token, cursor))
+    }
+
+    ///
+    pub fn lex(&mut self) -> Result<Vec<Token>, LexerError> {
+        //
+        let mut tokens = vec![];
+
+        //
+        while self.is_inbounds(None) {
+            let token = self.spaces();
+
+            if token.is_err() {
+                return Err(token.unwrap_err());
+            }
+
+            let token = token.unwrap();
+            if token.kind == TokenKind::Spaces {
+                tokens.push(token);
+            }
+        }
+
+        //
+        Ok(tokens)
+    }
 
     // fn peek_char(&mut self) -> Option<String>
 }
--- src/astro_test.rs
+++ src/astro_test.rs
@@ -1,19 +1,17 @@
 use astro_lexer::Lexer;
 
-
 fn main() {
     println!("Welcome to Astro!");
+
     let code = String::from(
         "  "
     );
+
     let lexer_result = Lexer::new(code).lex();
-    println!("lexer_result = {:?}", lexer_result)
+
+    println!("lexer_result = {:?}", lexer_result);
 }
 
-
-
-
-// const lexedResult = lex(
 // `  _  name for 0o3246\\\n ++ 0b100_1 # hello comment! \\\n'this is a string' 0b0
 // // -056670 >: 0x5654dead6 || regex he442ead ||0b0.1e-1<: <:name; ()0b0.1e-1 0o537e-7
 // # hello \\\n0x12_3p-25f 5657.0e-23\`a\`\"hello there\"'''\nThis is \nnew\n'''#-hello \n -# #-\n this is a #- nested\n-# comment -#
@@ -28,6 +26,3 @@ fn main() {
 //     __indent
 //         __indent_again
 // __dedent_dedent`
-// );
-
-// console.log(lexedResult);
