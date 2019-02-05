b7b73a0c99a123e5d05213842d1161192a8a4200
Steve Akinyemi <appcypher@outlook.com>
2019-02-05 14:00:36 +0100

    Change AST structure

--- lib/codegen/src/asts/asts.rs
+++ lib/codegen/src/asts/asts.rs
@@ -2,5 +2,12 @@ use astro_lexer::kinds::TokenKind;
 
 #[derive(Debug, Clone)]
 pub enum AST {
-    Terminal(TokenKind, String),
+    Terminal {
+        kind: TokenKind,
+        value: String,
+    },
+    Integer {
+        kind: TokenKind,
+        value: String,
+    }
 }
